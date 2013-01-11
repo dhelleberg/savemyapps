a35c6cb76f91e772422e7ad9e5405348b07a79dd
Dominik Helleberg <helleberg@gmx.de>
2013-01-11 22:36:32 +0100

    implemented crouton, kick the service when button is pressed, deselect item on cancel

--- android/src/org/cirrus/mobi/savemyapps/AppsListFragment.java
+++ android/src/org/cirrus/mobi/savemyapps/AppsListFragment.java
@@ -24,6 +24,9 @@ import java.util.List;
 import org.cirrus.mobi.savemyapps.data.App;
 import org.cirrus.mobi.savemyapps.service.HostCommService;
 
+import de.keyboardsurfer.android.widget.crouton.Crouton;
+
+import android.app.Notification.Style;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
@@ -34,6 +37,7 @@ import android.support.v4.app.LoaderManager;
 import android.support.v4.content.AsyncTaskLoader;
 import android.support.v4.content.Loader;
 import android.util.Log;
+import android.util.SparseBooleanArray;
 import android.view.ActionMode;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -58,25 +62,30 @@ public class AppsListFragment extends ListFragment implements LoaderManager.Load
 
 	public AppsListFragment() {
 	}
-	
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		mPackageManger = getActivity().getPackageManager();
 		getLoaderManager().initLoader(0, null, this);		
 	}
-	
+	@Override
+	public void onDestroy() {	
+		super.onDestroy();
+		Crouton.cancelAllCroutons();
+	}
+
 	@Override
 	public void onListItemClick(ListView l, View v, int position, long id) {	
 		super.onListItemClick(l, v, position, id);
-		
+
 		if(mMode == null)
 			mMode = getActivity().startActionMode(mActionModeCallback);
-		//testing: kick service
-		/*Intent serviceIntent = new Intent(getActivity().getApplicationContext(), HostCommService.class);
-		serviceIntent.putExtra(HostCommService.EXTRA_COMMAND, HostCommService.COMMAND_BACKUP_PACKAGE);
-		serviceIntent.putExtra(HostCommService.EXTRA_PARAMS, new String[]{mAdapter.appsList.get(position).packageName});
-		getActivity().startService(serviceIntent);*/
+		
+		//show crouton if user selects more then one app
+		if(getListView().getCheckedItemCount() == 2)		
+			Crouton.makeText(getActivity(), R.string.multiple_app_warning, de.keyboardsurfer.android.widget.crouton.Style.CONFIRM);
+	
 	}
 
 
@@ -139,24 +148,48 @@ public class AppsListFragment extends ListFragment implements LoaderManager.Load
 
 	private ActionMode.Callback mActionModeCallback = new ActionMode.Callback() {
 		// Called when the action mode is created; startActionMode() was called
-	    @Override
-	    public boolean onCreateActionMode(ActionMode mode, Menu menu) {
-	        // Inflate a menu resource providing context menu items
-	        MenuInflater inflater = mode.getMenuInflater();
-	        inflater.inflate(R.menu.apps_action_mode, menu);
-	        return true;
-	    }
+		@Override
+		public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+			// Inflate a menu resource providing context menu items
+			MenuInflater inflater = mode.getMenuInflater();
+			inflater.inflate(R.menu.apps_action_mode, menu);
+			return true;
+		}
 
 		@Override
 		public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
-			// TODO Auto-generated method stub
+			switch (item.getItemId()) {
+			case R.id.menu_backup:
+				//get all selected items
+				SparseBooleanArray items = getListView().getCheckedItemPositions();
+				String[]packageNames = new String[items.size()];
+				for (int i = 0; i < items.size(); i++)
+				{
+					packageNames[i] = mAdapter.appsList.get(items.keyAt(i)).packageName;
+				}
+				//kick the service
+				Intent serviceIntent = new Intent(getActivity().getApplicationContext(), HostCommService.class);
+				serviceIntent.putExtra(HostCommService.EXTRA_COMMAND, HostCommService.COMMAND_BACKUP_PACKAGE);
+				serviceIntent.putExtra(HostCommService.EXTRA_PARAMS, packageNames);
+				getActivity().startService(serviceIntent);
+				
+				break;
+
+			default:
+				break;
+			}
 			return false;
 		}
-		
+
 		@Override
 		public void onDestroyActionMode(ActionMode mode) {
-			// TODO Auto-generated method stub
-			
+			//de-select all items
+			SparseBooleanArray checked = getListView().getCheckedItemPositions();
+			for (int i = 0; i < checked.size(); i++)
+			{
+				getListView().setItemChecked(checked.keyAt(i), false);
+			}
+			mMode = null;
 		}
 
 		@Override
@@ -170,7 +203,7 @@ public class AppsListFragment extends ListFragment implements LoaderManager.Load
 	/**
 	 * Loader stuff 
 	 */
-	
+
 	public static class AppsListLoader extends AsyncTaskLoader<List<App>>
 	{
 		private List<App> mAppList;
